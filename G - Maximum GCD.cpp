ca3ed3eaa7d36a1fdea42423c869f3d9c5f5ea7f
rudra-404 <rudrakaiser840@gmail.com>
2024-08-21 18:26:01 +0600

    G - Maximum GCD

---
+++ G - Maximum GCD.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    int test, n;
+    cin >> test;
+    while (test--)
+    {
+        cin >> n;
+        cout << n / 2 << "\n";
+    }
+    return 0;
+}
