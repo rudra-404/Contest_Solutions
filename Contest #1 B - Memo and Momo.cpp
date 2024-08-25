23cb576afb7f739d9750fe967e48c47283233733
rudra-404 <rudrakaiser840@gmail.com>
2024-08-25 11:07:02 +0600

    ICPC Assiut Community

--- Contest #1 B - Memo and Momo.cpp
+++ Contest #1 B - Memo and Momo.cpp
@@ -5,9 +5,9 @@ int main()
     ios_base::sync_with_stdio(false);
     unsigned long long a, b, k;
     cin >> a >> b >> k;
-    if(a%k==0 & b%k==0) cout << "Both";
+    if(a%k==0 && b%k==0) cout << "Both";
     else if(a%k==0) cout << "Memo";
-    else if(b%k==0 & b%k==0) cout << "Momo";
+    else if(b%k==0) cout << "Momo";
     else cout << "No One";
     return 0;
 }
